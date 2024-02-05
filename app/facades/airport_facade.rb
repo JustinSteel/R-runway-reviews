3429e6f5455be5d0380ce2b9791a59913cc52906
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-02-05 14:03:32 -0700

    stash

--- app/facades/airport_facade.rb
+++ app/facades/airport_facade.rb
@@ -1,6 +1,9 @@
 class AirportFacade
   def self.airports
-    AirportService.airports.map do |airport_data|
+    require 'pry'; binding.pry
+    service = AirportService.new
+    service.airports.map do |airport_data|
+      require 'pry'; binding.pry
       Airport.new(airport_data)
     end
   end
--- app/services/airport_service.rb
+++ app/services/airport_service.rb
@@ -1,13 +1,13 @@
 class AirportService
   def airports
-    get_url("/v1/airports")
+    get_url("/v1/airports?country=us")
   end
   
   def get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
-
+  
   def conn
     Faraday.new(url: "https://api.api-ninjas.com") do |faraday|
       faraday.headers["X-Api-Key"] = Rails.application.credentials.airportkey[:key]
