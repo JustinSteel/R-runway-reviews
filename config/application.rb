0ff41676635037d3000a4f3b0e8819f8e4c65e99
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-02-08 11:31:25 -0700

    Add rack-cors gem, and configure in application file

--- config/application.rb
+++ config/application.rb
@@ -42,3 +42,15 @@ module RRunwayReviews
     config.api_only = true
   end
 end
+
+# config/initializers/cors.rb
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'  # Or set to specific domains
+    resource '*', 
+      headers: :any, 
+      methods: [:get, :post, :patch, :put]
+  end
+end
+
