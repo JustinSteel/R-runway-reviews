3f9bfc362613afbc601e6288a70824184571d9a8
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-02-09 14:06:38 -0700

    fix spelling error on amenities

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   validates_presence_of :comment, :category
   
-  enum category: {general: 0, security: 1, restaurants: 2, bathrooms: 3, ameneties: 4, accessability: 5}
+  enum category: {general: 0, security: 1, restaurants: 2, bathrooms: 3, amenities: 4, accessibility: 5}
 
   belongs_to :user
 end
